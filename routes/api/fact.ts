2c34eaf3faf33be695b4821f513b4715a886cf2c
Amine Braham <bra.ami20@hotmail.fr>
2023-04-23 17:15:23 +0100

    moved api key to env

--- routes/api/fact.ts
+++ routes/api/fact.ts
@@ -1,8 +1,10 @@
+import "https://deno.land/std@0.184.0/dotenv/load.ts";
+
 import { HandlerContext } from "$fresh/server.ts";
 import { Fact } from "../../components/Fact.model.tsx";
 
-//TODO: .env for api key
-const API_URL = 'https://api.nasa.gov/planetary/apod?api_key=AgKwrvmLriBfbeCyj6dXgV29s5bGRwOug8ZYVb3j&count=1'
+const API_KEY = Deno.env.get("API_KEY");
+const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=1`;
 
 export const handler = async (_req: Request, ctx: HandlerContext): Promise<Response> => {
     const resp = await fetch(API_URL);
